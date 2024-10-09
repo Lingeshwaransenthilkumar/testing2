e8a20b51a3cc00b9f228a122cf6c2856b3ff2901
Lingeshwaransenthilkumar <lingeshwaransenthilkumar0279@gmail.com>
2024-10-09 21:15:29 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useRef, useState } from "react";
 
-  return (
+function App(){
+  const [isDark,setIsDark] = useState(false);
+  const buttonRef = useRef(null);
+  function ToggleEvent(){
+    setIsDark(!isDark);
+    if(isDark){
+      buttonRef.current.style.backgroundColor = "white";
+      buttonRef.current.style.color = "black";
+    }
+    else{
+      buttonRef.current.style.backgroundColor = "black";
+      buttonRef.current.style.color = "white";
+    }
+  }
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div ref={buttonRef} className="container">
+        <h2 style={{color:"tomato"}}>hello world</h2>
+        <button  onClick={ToggleEvent}>Click me</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
 
-export default App
+export default App;
